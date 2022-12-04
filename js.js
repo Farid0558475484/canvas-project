03f3cb41ce43238c7ed3d701d767d82df1a295ca
Farid0558475484 <faridmh@code.edu.az>
2022-12-04 17:53:01 +0400

    first draw

--- js.js
+++ js.js
@@ -1,4 +1,23 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext("2d");
-ctx.fillRect(100, 50, 150, 75);
-ctx.fillStyle = "red";
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+
+
+canvas.width = 400;
+canvas.height = 200;
+
+
+context.fillStyle = "red";
+context.fillRect(100, 50, 150, 75);
+
+context.fillStyle = "blue";
+context.fillRect(150, 100, 100, 50);
+context.clearRect(0, 0, 400, 200);
+
+context.strokeStyle = "green";
+context.lineWidth = "10";
+context.rect(50,50,100,100) // x, y, width, height
+context.stroke()
+context.fillStyle = "yellow";
+context.fill()
+
+console.log("Hello World");
